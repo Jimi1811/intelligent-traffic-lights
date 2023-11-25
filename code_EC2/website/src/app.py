ab6a75c6d406ce0eff4e0f2e1a1d16bf911d0037
Ubuntu <ubuntu@ip-172-31-6-164.us-east-2.compute.internal>
2023-11-25 23:29:04 +0000

    website con sockets

--- code_EC2/website/src/app.py
+++ code_EC2/website/src/app.py
@@ -1,19 +1,29 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, Response
+import socket
+import socketio
 
 app = Flask(__name__)
-socketio = SocketIO(app)
+sio = socketio.Server(cors_allowed_origins='*')
+app.wsgi_app = socketio.WSGIApp(sio, app.wsgi_app)
+
+def frames_generator():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+        sock.connect(("172.20.10.4", 10001))
+        stream = sock.makefile("rb")
+        while True:
+            frame = stream.read(1024)  # Adjust buffer size as needed
+            if not frame:
+                break
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@socketio.on('video_frame')
-def handle_video_frame(frame):
-    # Procesa el frame como desees
-    print(f"Received video frame: {len(frame)} bytes")
-    # Envía el frame a todos los clientes conectados
-    socketio.emit('video_frame', frame)
+@app.route('/video_feed')
+def video_feed():
+    return Response(frames_generator(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000)
