89a5236277008e8cb8149bbc75ece9822778b751
Jim Fabián <jim.fabian@utec.edu.pe>
2023-11-30 02:30:38 -0500

    nuevo codigo

--- code_rasp-TFlite_bookwarm/jim/rpi_website/src/app.py
+++ code_rasp-TFlite_bookwarm/jim/rpi_website/src/app.py
@@ -5,26 +5,7 @@ from picamera2.encoders import JpegEncoder
 from picamera2.outputs import FileOutput
 from threading import Condition
 
-class CameraHandler:
-    def __init__(self):
-        self.picam2 = None
-        self.output = StreamingOutput()
-
-    def configure_camera(self):
-        if not self.picam2:
-            self.picam2 = Picamera2()
-            self.picam2.configure(self.picam2.create_video_configuration(main={"size": (640, 480)}))
-            self.picam2.start_recording(JpegEncoder(), FileOutput(self.output))
-
-    def get_frame(self):
-        with self.output.condition:
-            self.output.condition.wait()
-            frame = self.output.frame
-        return frame
-
-    def stop_recording(self):
-        if self.picam2:
-            self.picam2.stop_recording()
+app = Flask(__name__)
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -36,31 +17,31 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
-class WebApp:
-    def __init__(self, camera_handler):
-        self.app = Flask(__name__)
-        self.camera_handler = camera_handler
-        self.app.add_url_rule('/', 'index', self.index)
-        self.app.add_url_rule('/video_feed', 'video_feed', self.video_feed)
-        self.app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+output = StreamingOutput()
 
-    def index(self):
-        return render_template('index.html')
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-    def generate(self):
-        try:
-            while True:
-                self.camera_handler.configure_camera()
-                frame = self.camera_handler.get_frame()
-                yield (b'--frame\r\n'
-                       b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-        finally:
-            self.camera_handler.stop_recording()
+def generate():
+    while True:
+        with output.condition:
+            output.condition.wait()
+            frame = output.frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
-    def video_feed(self):
-        return Response(self.generate(),
-                        mimetype='multipart/x-mixed-replace; boundary=frame')
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    camera_handler = CameraHandler()
-    web_app = WebApp(camera_handler)
+    picam2 = picamera2.Picamera2()
+    picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+    picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+    try:
+        app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+    finally:
+        picam2.stop_recording()
