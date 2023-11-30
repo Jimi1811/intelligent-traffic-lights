acc09ef870baaa1c004311d2007a0cf610357015
Jim Fabián <jim.fabian@utec.edu.pe>
2023-11-30 00:35:42 -0500

    nuevo código

---
+++ code_rasp-TFlite_bookwarm/jim/rpi_website/src/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, render_template, Response
+import io
+import picamera2
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
+from threading import Condition
+
+app = Flask(__name__)
+
+class StreamingOutput(io.BufferedIOBase):
+    def __init__(self):
+        self.frame = None
+        self.condition = Condition()
+
+    def write(self, buf):
+        with self.condition:
+            self.frame = buf
+            self.condition.notify_all()
+
+output = StreamingOutput()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def generate():
+    while True:
+        with output.condition:
+            output.condition.wait()
+            frame = output.frame
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    picam2 = picamera2.Picamera2()
+    picam2.configure(picam2.create_video_configuration(main={"size": (640, 480)}))
+    picam2.start_recording(JpegEncoder(), FileOutput(output))
+
+    try:
+        app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
+    finally:
+        picam2.stop_recording()
