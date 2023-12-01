5cb0dc36a1492dc06db6ad7fd535113a21c5c5f7
Jim Fabián <jim.fabian@utec.edu.pe>
2023-12-01 17:45:36 -0500

    Arreglando git

--- code_EC2/website/src/app.py
+++ code_EC2/website/src/app.py
@@ -54,4 +54,4 @@ def service():
 # Crear funcion
 if __name__ == '__main__':
     # Correr programa, hacer debug true, especificar puerto (default: 5000)
-    app.run(port=5000,debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
