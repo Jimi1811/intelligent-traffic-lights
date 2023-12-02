588e4476a016f06e48a6f1d93419587789182cda
Jim Fabián <jim.fabian@utec.edu.pe>
2023-12-02 02:40:38 -0500

    Cambio de streaming

--- code_raspberry/streaming_2/src/app.py
+++ code_raspberry/streaming_2/src/app.py
@@ -52,4 +52,4 @@ def video_feed():
     return Response(generate(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', port=6000, debug=True)
