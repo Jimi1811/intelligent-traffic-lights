7c18d573b2399293150634a4595f9b1d6ddb2f18
Jim Fabián <jim.fabian@utec.edu.pe>
2023-12-02 02:17:54 -0500

    Mejorando la grabacion

--- code_raspberry/streaming/src/app.py
+++ code_raspberry/streaming/src/app.py
@@ -1,47 +1,11 @@
 from flask import Flask, render_template, Response
 import io
 from picamera2 import Picamera2
-from picamera2.encoders import JpegEncoder, H264Encoder
-from picamera2.outputs import FileOutput, FfmpegOutput
+from picamera2.encoders import JpegEncoder
+from picamera2.outputs import FileOutput
 from threading import Condition
-import time
-from datetime import datetime
 
-class Camera:
-    def __init__(self):
-        self.camera = Picamera2()
-        self.camera.configure(self.camera.create_video_configuration(main={"size": (640, 480)}))
-        self.encoder = JpegEncoder()
-        self.file_out = FfmpegOutput('test2.mp4', audio=False)  # StreamingOutput()
-        self.stream_out = StreamingOutput()
-        self.stream_out2 = FileOutput(self.stream_out)
-        self.encoder.output = [self.file_out, self.stream_out2]
-
-        self.camera.start_encoder(self.encoder)
-        self.camera.start()
-
-        self.recording = False  # Agregamos un indicador de grabación
-
-    def start_recording(self):
-        if not self.recording:
-            current_time = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-            video_filename = f"{current_time}.mp4"
-            self.file_out.filename = video_filename
-            self.camera.start_recording(self.encoder, self.file_out)
-            self.recording = True
-
-    def stop_recording(self):
-        if self.recording:
-            self.camera.stop_recording()
-            self.recording = False
-
-    def get_frame(self):
-        self.camera.start()
-        with self.stream_out.condition:
-            self.stream_out.condition.wait()
-            frame = self.stream_out.frame
-        return frame
-
+app = Flask(__name__)
 
 class StreamingOutput(io.BufferedIOBase):
     def __init__(self):
@@ -53,35 +17,34 @@ class StreamingOutput(io.BufferedIOBase):
             self.frame = buf
             self.condition.notify_all()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# defines the function that generates our frames
-camera = Camera()
+def generate():
+    picam2 = Picamera2()
+    picam2.preview_configuration.main.size = (480, 320)
+    picam2.preview_configuration.main.format = "RGB888"
+    picam2.preview_configuration.controls.FrameRate = 30
+    picam2.preview_configuration.align()
+    picam2.configure("preview")    
+    output = StreamingOutput()  
+    picam2.start_recording(JpegEncoder(), FileOutput(output))
 
+    try:
+        while True:
+            with output.condition:
+                output.condition.wait()
+                frame = output.frame
+            yield (b'--frame\r\n'
+                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    finally:
+        picam2.stop_recording()
 
-def gen_frames():
-    while True:
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
-        time.sleep(10)  # Espera 10 segundos entre cada frame
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate(),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
 
-# Inicia la grabación al ejecutar el script
-camera.start_recording()
-
-try:
-    # Inicia la aplicación Flask
-    app = Flask(__name__)
-
-    @app.route('/')
-    def index():
-        return render_template('index.html')
-
-    @app.route('/video_feed')
-    def video_feed():
-        return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-    app.run(debug=False, host='0.0.0.0', port=5000)
-
-finally:
-    # Detén la grabación cuando la aplicación Flask se detiene
-    camera.stop_recording()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, debug=True, threaded=True)
